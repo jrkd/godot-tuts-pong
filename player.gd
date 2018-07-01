395f02d6fffda127047c1749ea833c2c44250f50
jrkd <jono@jrkd.co.uk>
2018-07-01 17:36:53 +0100

    Players now have their own inputs.
    Still need to setup menu and restart screens, reorder the tree structure

--- player.gd
+++ player.gd
@@ -4,19 +4,22 @@ extends Node2D
 # var a = 2
 # var b = "textvar"
 
-export  (int) var speed = 100
+export  (int) var speed = 500
+
+export (String) var UP_INPUT_ACTION = "player1-up"	 
+export (String) var DOWN_INPUT_ACTION = "player1-down"
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	print(UP_INPUT_ACTION)
 	pass
 
 func _process(delta):
-	
 	var velocity = Vector2() # the player's movement vector
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed(DOWN_INPUT_ACTION):
 		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed(UP_INPUT_ACTION):
 		velocity.y -= 1
 	
 	position += velocity*delta*speed
