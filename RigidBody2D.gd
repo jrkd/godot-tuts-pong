fdeb9a36cd50f96cc7dde327c38600fe1ec7f610
jrkd <jono@jrkd.co.uk>
2018-07-02 00:02:36 +0100

    Modified the pong block script so that things got reset properly when someone won.
    Had some real issues figuring out exactly what you need to modify and also __when__ when your talking about physics manipulation.

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -5,27 +5,29 @@ signal gameover
 
 
 export (Vector2) var initialVector = Vector2(250,-250)
-var appliedInitial = false
-var blockCanMove = false
 var initialPosition = Vector2()
+
+var applyInitial = false
+var resetTransform = false
+
 func _ready():
-	initialPosition = position
+	initialPosition = get_global_transform().origin
 
 func start():
-	reset()
-	blockCanMove = true 
-	
-func reset():
-	blockCanMove = false
-	appliedInitial = false
-	position = initialPosition
-	set_linear_velocity(Vector2())	
+	applyInitial = true
 
+# You need to do any physics messing within this method against 
+# physics state - they won't work anywhere else. Hence just setting booleans in places.
 func _integrate_forces(state):
-	if appliedInitial == false and blockCanMove:
-		appliedInitial = true
-		set_linear_velocity(initialVector)
-
+	if resetTransform:
+		state.linear_velocity = Vector2()
+		state.transform = Transform2D(0.0, initialPosition)
+		resetTransform = false
+		
+	if applyInitial:
+		applyInitial = false
+		state.linear_velocity = initialVector
+	
 func _on_blockRigidBody2D_body_entered(body):
 	if body.get_name() == "left" ||  body.get_name() == "right":
 		if body.get_name() == "left":
@@ -34,7 +36,7 @@ func _on_blockRigidBody2D_body_entered(body):
 			emit_signal('outOfBoundsRight')
 		
 		# Pause the block moving
-		reset()
+		resetTransform = true
 		
 		# notify other objects that it's game over!
 		emit_signal('gameover')
